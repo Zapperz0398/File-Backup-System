a696c24693acb22ac083e8edf5b6ccdb1bce8db9
Zapperz0398 <electricityman1987@gmail.com>
2024-03-19 19:42:06 +0200

    Create folder copier

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def map_root_file_system(root_dir : str):
     file_system_map = {}
 
     for dirpath, dirname, filename in os.walk(root_dir):
-        pass
+        if "C:\\" in dirpath:
+            dirpath = dirpath.removeprefix("C:\\")
+            dirpath = r"C:\Backups" + "\\" + dirpath + "\\"
+        else:
+            print("Error choosing file destination - the program does not support file choosing yet")
+            
+        os.mkdir(dirpath)
 
 map_root_file_system("C:\PDM")
