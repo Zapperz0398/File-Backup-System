ea1f8a32101bec7cd2db4cf2f4e6966e9c9c500a
Zapperz0398 <electricityman1987@gmail.com>
2024-03-21 16:39:54 +0200

    seperate directory copier from file_map function

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ def map_root_file_system(root_dir : str):
             dirpath = r"C:\Backups" + "\\" + dirpath + "\\"
         else:
             print("Error choosing file destination - the program does not support file choosing yet")
-            
-        os.mkdir(dirpath)
+        
+
+        # Current issue right here is that there is no folder 'C:\Backups' in the initial startup. Using try except does not solve the problem
+
+def make_directories(file_map):
+    for path in file_map:
+        os.mkdir(path)
+
 
 map_root_file_system("C:\PDM")
