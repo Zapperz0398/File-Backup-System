1ab6462a890aa96b2670858db27369a3a526b235
Zapperz0398 <electricityman1987@gmail.com>
2024-03-21 20:28:16 +0200

    Implement full functionality for file path mapper & Fix Main directory bug & linted code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 
+
 def establish_database_connection(database_name):
     """
     Establishes a connection to the database.
@@ -15,7 +16,8 @@ def establish_database_connection(database_name):
 
     return database, database_cursor
 
-def map_root_file_system(root_dir : str):
+
+def map_root_file_system(root_dir: str):
     """
     Maps the entire file system from the root directory.
 
@@ -26,22 +28,26 @@ def map_root_file_system(root_dir : str):
     
     """
 
-    file_system_map = {}
+    file_system_map = {"Directories": ["C:\\Backup\\"], "Files": []}
 
     for dirpath, dirname, filename in os.walk(root_dir):
         if "C:\\" in dirpath:
             dirpath = dirpath.removeprefix("C:\\")
             dirpath = r"C:\Backups" + "\\" + dirpath + "\\"
         else:
-            print("Error choosing file destination - the program does not support file choosing yet")
+            print("Error - The program does not support file choosing yet")
         
+        file_system_map["Directories"] += [dirpath]
 
-        # Current issue right here is that there is no folder 'C:\Backups' in the initial startup. Using try except does not solve the problem
+        for file in filename:
+            filepath = r"C:\Backups" + "\\" + dirpath + file
+            file_system_map["Files"] += [filepath]
+
 
 def make_directories(file_map):
     for path in file_map:
         os.mkdir(path)
 
 
-database, cursor = establish_database_connection("database-map.db")
+# database, cursor = establish_database_connection("database-map.db")
 map_root_file_system("C:\PDM")
