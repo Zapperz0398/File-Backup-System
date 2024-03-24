7c7aac1f29610ea2bd59b584e2fbd60c99d1f76f
Zapperz0398 <electricityman1987@gmail.com>
2024-03-24 21:23:25 +0200

    Fix all syntax issues

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import sqlite3
 
 
 def establish_database_connection(database_name):
+    # Update necessary in docstrings to required
     """
-    Establishes a connection to the database. Creates a database file if none exists with necessary tables
+    Establishes a connection to the database.
+    Creates a database file if none exists with necessary tables
 
     Parameters:
     - database_name: Name of the database to connect to
@@ -42,7 +44,7 @@ def establish_database_connection(database_name):
 
 def database_table_exists(database_cursor, table_name):
     """
-    Checks to see if a table exists in a database using a databases cursor object
+    Checks if a table exists in a database using a databases cursor object
 
     Parameters:
     - databasae_cursor: The cursor the database is associated with
@@ -67,10 +69,10 @@ def create_database_tables(database_cursor):
 
     Parameters:
     - database_cursor: The cursor the database is associated with
-    
+
     Returns: None
     """
-    
+
     database_cursor.execute("""CREATE TABLE Directories (
                                 name TEXT PRIMARY KEY,
                                 path TEXT)
@@ -110,11 +112,12 @@ def map_root_file_system(root_dir: str):
             file_system_map["Files"][filepath]["creation_time"] = creation_time
             file_system_map["Files"][filepath]["last_modified"] = last_modified
             file_system_map["Files"][filepath]["byte_size"] = file_size
-            
+
+
 def make_directories(file_map):  # Update syntax
     for path in file_map:
         os.mkdir(path)
 
 
 database, cursor = establish_database_connection("database-map.db")
-map_root_file_system("C:\PDM")
+map_root_file_system(r"C:\PDM")
