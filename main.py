26895f4fa5d267d646e50c9a98d044c2ac0ae555
Zapperz0398 <electricityman1987@gmail.com>
2024-06-19 21:13:39 +0200

    Refactor file to use tabs instead of spaces

--- main.py
+++ main.py
@@ -6,129 +6,129 @@ from dotenv import load_dotenv
 
 
 def establish_database_connection(database_name):
-    """
-    Establishes a connection to the database.
-    Creates a database file if none exists with required tables
+	"""
+	Establishes a connection to the database.
+	Creates a database file if none exists with required tables
 
-    Parameters:
-    - database_name: Name of the database to connect to
-    """
+	Parameters:
+	- database_name: Name of the database to connect to
+	"""
 
-    if not os.path.exists(database_name):
-        with open(database_name, "x", encoding = "utf-8") as _:
-            pass
+	if not os.path.exists(database_name):
+		with open(database_name, "x", encoding = "utf-8") as _:
+			pass
 
-        database = sqlite3.connect(database_name)
-        database_cursor = database.cursor()
+		database = sqlite3.connect(database_name)
+		database_cursor = database.cursor()
 
-        create_database_tables(database_cursor)
-    else:
-        database = sqlite3.connect(database_name)
-        database_cursor = database.cursor()
+		create_database_tables(database_cursor)
+	else:
+		database = sqlite3.connect(database_name)
+		database_cursor = database.cursor()
 
-        if not database_table_exists(database_cursor, "Directories"):
-            database_cursor.execute("""CREATE TABLE Directories (
-                                        ID int PRIMARY KEY,
-                                        name Text,
-                                        path TEXT)
-            """)
+		if not database_table_exists(database_cursor, "Directories"):
+			database_cursor.execute("""CREATE TABLE Directories (
+										ID int PRIMARY KEY,
+										name Text,
+										path TEXT)
+			""")
 
-        if not database_table_exists(database_cursor, "Files"):
-            database_cursor.execute("""CREATE TABLE Files (
-                                ID int PRIMARY KEY,
-                                name TEXT,
-                                path TEXT,
-                                byte_size int,
-                                created TEXT,
-                                last_modified TEXT)
-            """)
+		if not database_table_exists(database_cursor, "Files"):
+			database_cursor.execute("""CREATE TABLE Files (
+								ID int PRIMARY KEY,
+								name TEXT,
+								path TEXT,
+								byte_size int,
+								created TEXT,
+								last_modified TEXT)
+			""")
 
-    return database
+	return database
 
 
 def database_table_exists(database_cursor, table_name):
-    """
-    Checks if a table exists in a database using a databases cursor object
+	"""
+	Checks if a table exists in a database using a databases cursor object
 
-    Parameters:
-    - databasae_cursor: The cursor the database is associated with
-    - table_name: The name of the table to check if it exists
+	Parameters:
+	- databasae_cursor: The cursor the database is associated with
+	- table_name: The name of the table to check if it exists
 
-    Returns: bool
-    """
+	Returns: bool
+	"""
 
-    table = database_cursor.execute(f"""SELECT name FROM sqlite_master
-                                        WHERE type = 'table'
-                                        AND name = '{table_name}'""")
+	table = database_cursor.execute(f"""SELECT name FROM sqlite_master
+										WHERE type = 'table'
+										AND name = '{table_name}'""")
 
-    if table.fetchone is None:
-        return False
-    else:
-        return True
+	if table.fetchone is None:
+		return False
+	else:
+		return True
 
 
 def create_database_tables(database_cursor):
-    """
-    Creates the tables for the applications database
+	"""
+	Creates the tables for the applications database
 
-    Parameters:
-    - database_cursor: The cursor the database is associated with
+	Parameters:
+	- database_cursor: The cursor the database is associated with
 
-    Returns: None
-    """
+	Returns: None
+	"""
 
-    database_cursor.execute("""CREATE TABLE Directories (
-                                ID int PRIMARY KEY,
-                                name Text,
-                                path TEXT)
-    """)
+	database_cursor.execute("""CREATE TABLE Directories (
+								ID int PRIMARY KEY,
+								name Text,
+								path TEXT)
+	""")
 
-    database_cursor.execute("""CREATE TABLE Files (
-                                ID int PRIMARY KEY,
-                                name TEXT,
-                                path TEXT,
-                                byte_size int,
-                                created TEXT,
-                                last_modified TEXT)
-    """)
+	database_cursor.execute("""CREATE TABLE Files (
+								ID int PRIMARY KEY,
+								name TEXT,
+								path TEXT,
+								byte_size int,
+								created TEXT,
+								last_modified TEXT)
+	""")
 
 
 def map_root_file_system(root_dir: str):
-    """
-    Maps the entire file system from the chosen directory.
+	"""
+	Maps the entire file system from the chosen directory.
 
-    Parameters:
-    root_dir: The root directory to map
+	Parameters:
+	root_dir: The root directory to map
 
-    Returns: file_map (list)
-    """
+	Returns: file_map (list)
+	"""
 
-    os.system(f"find {root_dir} > filemap.txt")
+	os.system(f"find {root_dir} > filemap.txt")
 
-    file_system_map = {"Directories": [], "Files": []}
-    directory_id = 0
-    file_id = 0
+	file_system_map = {"Directories": [], "Files": []}
+	directory_id = 0
+	file_id = 0
 
-    with open("filemap.txt", "r", encoding = "utf-8") as file:
-    	for line in file:
-    	    path = line.strip()
+	with open("filemap.txt", "r", encoding = "utf-8") as file:
+		for line in file:
+			path = line.strip()
 
-    	    if os.path.isdir(path):
-    	        directory_id += 1
+			if os.path.isdir(path):
+				directory_id += 1
 
-    	        directory_data = {"ID": directory_id, "path": path}
-    	        file_system_map["Directories"] += directory_data
-    	    elif os.path.isfile(path):
-    	        file_id += 1
+				directory_data = {"ID": directory_id, "path": path}
+				file_system_map["Directories"] += directory_data
+			elif os.path.isfile(path):
+				file_id += 1
 
-    	        file_metadata = os.stat(path)
-    	        file_last_modified_time = file_metadata.st_mtime
-    	        file_size = file_metadata.st_size
+				file_metadata = os.stat(path)
+				file_last_modified_time = file_metadata.st_mtime
+				file_size = file_metadata.st_size
 
-    	        file_data = {"ID": file_id, "path": path, "last_modfied": file_last_modified_time, "size": file_size}
-    	        file_system_map["Files"] += file_data
+				file_data = {"ID": file_id, "path": path, "last_modfied": file_last_modified_time, "size": file_size}
+				file_system_map["Files"] += file_data
 
-    return file_system_map
+	return file_system_map
 
 
 load_dotenv(".env")
