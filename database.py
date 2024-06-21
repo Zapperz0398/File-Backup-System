6e81236f5c606cf74ac23246512a00092c2b9b6c
Zapperz0398 <electricityman1987@gmail.com>
2024-06-21 17:31:25 +0200

    Seperate database functionality into its own file

---
+++ database.py
@@ -0,0 +1,121 @@
+"""This file contains all the functions related to local database interaction & minipulation"""
+
+import os
+import sqlite3
+
+
+def establish_database_connection(database_name):
+	"""
+	Establishes a connection to the database.
+	Creates a database file if none exists with required tables
+
+	Parameters:
+	- database_name: Name of the database to connect to
+	"""
+
+	if not os.path.exists(database_name):
+		with open(database_name, "x", encoding = "utf-8") as _:
+			pass
+
+		database = sqlite3.connect(database_name)
+		database_cursor = database.cursor()
+
+		create_database_tables(database_cursor)
+	else:
+		database = sqlite3.connect(database_name)
+		database_cursor = database.cursor()
+
+		if not database_table_exists(database_cursor, "Directories"):
+			database_cursor.execute("""CREATE TABLE Directories (
+										ID int PRIMARY KEY,
+										path TEXT)
+			""")
+
+		if not database_table_exists(database_cursor, "Files"):
+			database_cursor.execute("""CREATE TABLE Files (
+										ID int PRIMARY KEY,
+										path TEXT,
+										last_modified TEXT,
+										byte_size int)
+			""")
+
+	return database
+
+
+def database_table_exists(database_cursor, table_name):
+	"""
+	Checks if a table exists in a database using a databases cursor object
+
+	Parameters:
+	- databasae_cursor: The cursor the database is associated with
+	- table_name: The name of the table to check if it exists
+
+	Returns: bool
+	"""
+
+	table = database_cursor.execute(f"""SELECT name FROM sqlite_master
+										WHERE type = 'table'
+										AND name = '{table_name}'""")
+
+	if table.fetchone is None:
+		return False
+	return True
+
+
+def create_database_tables(database_cursor):
+	"""
+	Creates the tables for the applications database
+
+	Parameters:
+	- database_cursor: The cursor the database is associated with
+
+	Returns: None
+	"""
+
+	database_cursor.execute("""CREATE TABLE Directories (
+								ID int PRIMARY KEY,
+								path TEXT)
+	""")
+
+	database_cursor.execute("""CREATE TABLE Files (
+								ID int PRIMARY KEY,
+								path TEXT,
+								last_modified TEXT,
+								byte_size int)
+	""")
+
+
+def add_data_to_database(database, data):
+	"""
+	Adds a mapped file-system data set to a local database
+	
+	Parameters:
+	- database: A local database connection object
+	- data: A mapped file-system data set
+	
+	Returns: None
+	"""
+
+	database_cursor = database.cursor()
+
+	for data_set in data["Directories"]:
+		directory_id = data_set["ID"]
+		directory_path = data_set["path"]
+
+		db_query = """INSERT INTO 'Directories' (ID, path)
+			VALUES (?, ?)"""
+		database_cursor.execute(db_query, (directory_id, directory_path))
+		database.commit()
+
+	for data_set in data["Files"]:
+		file_id = data_set["ID"]
+		file_path = data_set["path"]
+		file_last_modified_time = data_set["last_modified"]
+		file_size = data_set["size"]
+
+		db_query = """INSERT INTO 'Files' (ID, path, last_modified, byte_size)
+			VALUES (?, ?, ?, ?)"""
+		database_cursor.execute(db_query, (file_id, file_path, file_last_modified_time, file_size))
+		database.commit()
+
+	database.close()
--- main.py
+++ main.py
@@ -1,125 +1,9 @@
 """This file is the main file for running all resources & functions to backup files"""
 
 import os
-import sqlite3
 from dotenv import load_dotenv
 
-
-def establish_database_connection(database_name):
-	"""
-	Establishes a connection to the database.
-	Creates a database file if none exists with required tables
-
-	Parameters:
-	- database_name: Name of the database to connect to
-	"""
-
-	if not os.path.exists(database_name):
-		with open(database_name, "x", encoding = "utf-8") as _:
-			pass
-
-		database = sqlite3.connect(database_name)
-		database_cursor = database.cursor()
-
-		create_database_tables(database_cursor)
-	else:
-		database = sqlite3.connect(database_name)
-		database_cursor = database.cursor()
-
-		if not database_table_exists(database_cursor, "Directories"):
-			database_cursor.execute("""CREATE TABLE Directories (
-										ID int PRIMARY KEY,
-										path TEXT)
-			""")
-
-		if not database_table_exists(database_cursor, "Files"):
-			database_cursor.execute("""CREATE TABLE Files (
-										ID int PRIMARY KEY,
-										path TEXT,
-										last_modified TEXT,
-										byte_size int)
-			""")
-
-	return database
-
-
-def database_table_exists(database_cursor, table_name):
-	"""
-	Checks if a table exists in a database using a databases cursor object
-
-	Parameters:
-	- databasae_cursor: The cursor the database is associated with
-	- table_name: The name of the table to check if it exists
-
-	Returns: bool
-	"""
-
-	table = database_cursor.execute(f"""SELECT name FROM sqlite_master
-										WHERE type = 'table'
-										AND name = '{table_name}'""")
-
-	if table.fetchone is None:
-		return False
-	return True
-
-
-def create_database_tables(database_cursor):
-	"""
-	Creates the tables for the applications database
-
-	Parameters:
-	- database_cursor: The cursor the database is associated with
-
-	Returns: None
-	"""
-
-	database_cursor.execute("""CREATE TABLE Directories (
-								ID int PRIMARY KEY,
-								path TEXT)
-	""")
-
-	database_cursor.execute("""CREATE TABLE Files (
-								ID int PRIMARY KEY,
-								path TEXT,
-								last_modified TEXT,
-								byte_size int)
-	""")
-
-
-def add_data_to_database(database, data):
-	"""
-	Adds a mapped file-system data set to a local database
-	
-	Parameters:
-	- database: A local database connection object
-	- data: A mapped file-system data set
-	
-	Returns: None
-	"""
-
-	database_cursor = database.cursor()
-
-	for data_set in data["Directories"]:
-		directory_id = data_set["ID"]
-		directory_path = data_set["path"]
-
-		db_query = """INSERT INTO 'Directories' (ID, path)
-			VALUES (?, ?)"""
-		database_cursor.execute(db_query, (directory_id, directory_path))
-		database.commit()
-
-	for data_set in data["Files"]:
-		file_id = data_set["ID"]
-		file_path = data_set["path"]
-		file_last_modified_time = data_set["last_modified"]
-		file_size = data_set["size"]
-
-		db_query = """INSERT INTO 'Files' (ID, path, last_modified, byte_size)
-			VALUES (?, ?, ?, ?)"""
-		database_cursor.execute(db_query, (file_id, file_path, file_last_modified_time, file_size))
-		database.commit()
-
-	database.close()
+import database
 
 
 def map_root_file_system(root_dir: str):
@@ -167,7 +51,7 @@ def map_root_file_system(root_dir: str):
 load_dotenv(".env")
 ROOT = os.getenv("ROOT_DIR")
 
-DATABASE = establish_database_connection("database-map.db")
+DATABASE = database.establish_database_connection("database-map.db")
 ROOT_FILE_MAP = map_root_file_system(ROOT)
 
-add_data_to_database(DATABASE, ROOT_FILE_MAP)
+database.add_data_to_database(DATABASE, ROOT_FILE_MAP)
