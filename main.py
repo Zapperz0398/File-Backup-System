1389347b56afdbc6139379fe04be9455bf0f03d7
Zapperz0398 <electricityman1987@gmail.com>
2024-06-21 22:45:38 +0200

    Fix missing arguement & no return warnings

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def map_root_file_system(root_dir: str):
 load_dotenv(".env")
 ROOT = os.getenv("ROOT_DIR")
 
-DATABASE = database.establish_database_connection("database-map.db")
+database.establish_database_connection("database-map.db")
 ROOT_FILE_MAP = map_root_file_system(ROOT)
 
-database.add_data_to_database(DATABASE, ROOT_FILE_MAP)
+database.add_data_to_database(ROOT_FILE_MAP)
